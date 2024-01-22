72b9c605705758cc5af633ed696c0244206e1ecd
Sabeeh Tahir <sabeeh7555@icloud.com>
2024-01-22 18:14:21 -0500

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,14 +11,14 @@ export const Footer = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col sm={6}>
-                        <img src={logo} alt="logo" className="footer-logo"/>
+                        <a href="#home"><img src={logo} alt="logo" className="footer-logo"/></a>
                     </Col>
                     <Col sm={6} className="text-center text-sm-end social-right">
                         <div className="social-icon">
-                            <a href="#"><img src={linkedin} alt="LinkedIn"/></a>
-                            <a href="#"><img src={github} alt="GitHub"/></a>
-                            <a href="#"><img src={facebook} alt="FaceBook"/></a>
-                            <a href="#"><img src={twitter} alt="X"/></a>
+                            <a target="_blank" href="https://www.linkedin.com/in/sabeeh-t-a20588130/"><img src={linkedin} alt="LinkedIn"/></a>
+                            <a target="_blank" href="https://github.com/Zaybeeh"><img src={github} alt="GitHub"/></a>
+                            <a target="_blank" href="https://www.facebook.com/sabeeh.tahir.5/"><img src={facebook} alt="FaceBook"/></a>
+                            <a target="_blank" href="https://twitter.com/sabeeh7555"><img src={twitter} alt="X"/></a>
                         </div>
                         <p>CopyRight 2023. All Rights Reserved by Sabeeh Tahir</p>
                     </Col>
