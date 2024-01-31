1a2838a4ca807cb9df2c00d7b5637939c4cd4d33
Sabeeh <sabeeh7555@icloud.com>
2024-01-31 18:06:11 -0500

    minor tweaks

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [delta, setDelta] = useState(1 - Math.random() * 100);
     const [index, setIndex] = useState(1);
     const toRotate = [ "Automation Engineer", "Coop Student", "Software Developer"];
     const period = 2000;
@@ -41,7 +41,7 @@ export const Banner = () => {
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
             setIndex(1);
-            setDelta(500)
+            setDelta(300)
         }
     }
 
